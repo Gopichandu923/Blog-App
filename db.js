e2cfd73879d2a1d2a443375087eba8f2837f977f
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-04 16:40:13 +0530

    Created the pool for database operations and exported

---
+++ db.js
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+import dotEnv from "dotenv";
+
+dotEnv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
