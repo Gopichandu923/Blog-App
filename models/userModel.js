77a81fa3bed10b691f37c358772697e0bb7fe82b
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-05 18:49:51 +0530

    updated the usermodel.

--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,27 @@
 import db from "../config/db.js";
 
-const { query } = db;
-
-export const findUserByEmailOrUsername = async (identifier) => {
-  const result = await query(
-    "SELECT * FROM users WHERE username=$1 OR email=$2",
-    [identifier, identifier]
-  );
+export const findUserByUsername = async (username) => {
+  const result = await db.query("SELECT * FROM users WHERE username=$1", [
+    username,
+  ]);
   return result.rows[0];
 };
 
+export const findUserById = async (id) => {
+  const result = await db.query("SELECT id FROM users WHERE id=$1", [id]);
+  return result.rowCount === 1;
+};
+
 export const findUserByEmail = async (email) => {
-  const result = await query(
-    "SELECT id,username,email,created_at FROM  users WHERE email=$1",
+  const result = await db.query(
+    "SELECT id,username,email,created_at,password FROM  users WHERE email=$1",
     [email]
   );
   return result.rows[0];
 };
 
 export const createUser = async (username, email, password) => {
-  const result = await query(
+  const result = await db.query(
     "INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING id,username,email",
     [username, email, password]
   );
