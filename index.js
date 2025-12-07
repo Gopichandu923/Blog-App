24218ad6aff891c76d4623a28201fa3117c78722
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-07 10:24:03 +0530

    Included the user auth in the server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
+import authRouter from "./routes/user.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRouter);
+
 const PORT = process.env.PORT_NUM;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Blog App.");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
