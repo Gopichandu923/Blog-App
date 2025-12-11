fadb2e4eba850f89ff5453a18283c245ce753e08
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-11 23:50:04 +0530

    Created the middlewares for authorization for posts and comments.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,12 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { findUserById } from "../models/userModel.js";
+import { getPostById } from "../models/postModel.js";
+import { getCommentById } from "../models/commentModel.js";
 
 dotenv.config();
 
+//TO verify the user by using JWT token
 export const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
@@ -34,5 +37,104 @@ export const protect = async (req, res, next) => {
     }
     console.log(error);
     return res.status(500).json({ message: "Invalid token." });
+  }
+};
+
+//  Post Authorization (PUT /posts/:id, DELETE /posts/:id)
+export const authorizePostOwner = async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+  try {
+    const post = await getPostById(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+    const isPostAuthor = post.user_id === userId;
+    if (!isPostAuthor) {
+      if (req.method === "PUT")
+        return res.status(403).json({
+          message: "Post can only be modified by the post author.",
+        });
+      if (req.method === "DELETE")
+        return res.status(403).json({
+          message: "Post can only be delete by the post author.",
+        });
+    } else return next();
+  } catch (error) {
+    console.error("Authorization error for post deletion:", error);
+    if (error.code === "22P02") {
+      return res
+        .status(404)
+        .json({ message: "Post not found (Invalid ID format)." });
+    }
+    res
+      .status(500)
+      .json({ message: "Server error during authorization check." });
+  }
+};
+//  Comment Update Authorization (PUT /comments/:id)
+export const authorizeCommentOwner = async (req, res, next) => {
+  const commentId = req.params.id;
+  const userId = req.user.id;
+
+  try {
+    const comment = await getCommentById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found." });
+    }
+    const isCommentOwner = comment.user_id === userId;
+    if (!isCommentOwner) {
+      return res.status(403).json({
+        message: "Comment can only be modified by the comment owner.",
+      });
+    }
+    return next();
+  } catch (error) {
+    console.error("Authorization error for comment deletion:", error);
+    if (error.code === "22P02") {
+      return res
+        .status(404)
+        .json({ message: "Comment not found (Invalid ID format)." });
+    }
+    res
+      .status(500)
+      .json({ message: "Server error during authorization check." });
+  }
+};
+
+export const authorizeCommentDeletion = async (req, res, next) => {
+  const commentId = req.params.id;
+  const userId = req.user.id;
+
+  try {
+    const comment = await getCommentById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found." });
+    }
+
+    const postAuthorId = await getPostById(comment.post_id);
+
+    const isCommentOwner = comment.user_id === userId;
+    const isPostAuthor = postAuthorId.user_id === userId;
+    if (isCommentOwner || isPostAuthor) {
+      return next();
+    } else {
+      return res.status(403).json({
+        message:
+          "Comment can only be deleted by the comment owner or the post author.",
+      });
+    }
+  } catch (error) {
+    console.error("Authorization error for comment deletion:", error);
+    if (error.code === "22P02") {
+      return res
+        .status(404)
+        .json({ message: "Comment not found (Invalid ID format)." });
+    }
+    res
+      .status(500)
+      .json({ message: "Server error during authorization check." });
   }
 };
