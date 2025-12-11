a78c1f4cde2fd85ef6c133ccd3b23a2d0b1a433c
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-11 11:47:34 +0530

    Created the routes for comment update and deletion.

--- routes/comment.js
+++ routes/comment.js
@@ -6,4 +6,10 @@ const router = express.Router();
 
 router.post("/", protect, commentController.createComment);
 router.get("/{post_id}", protect, commentController.getAllComments);
+router
+  .route("/:id")
+  .get(protect, commentController.getComment)
+  .put(protect, commentController.updateComment)
+  .delete(protect, commentController.deleteComment);
+
 export default router;
